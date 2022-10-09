2d987b01f6753a7c1fa9a89025f89f6175866a0e
Evgeniy_Shabolin <steamguardemail@yandex.ru>
2022-10-10 00:28:35 +0300

    complete meteo and forecast

--- forecast.py
+++ forecast.py
@@ -7,16 +7,12 @@ res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
                    params={'q': city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
 data = res.json()
 
-#print("Прогноз погоды на неделю:")
-#for i in data['list']:
-#    print('<Дата: ',i['dt_txt'], '>\r\nТемпература <',
-#          i['main']['temp'], '>')
-
-
 for i in data['list']:
-    print("Дата <", i['dt_txt'],
-          "> \r\nТемпература <", '{0:+3.0f}'.format(i['main']['temp']),
+    print(
+        "--------------------------------",
+        "\r\nДата <", i['dt_txt'],
+          "> \r\nТемпература <", '{0:+3.0f}'.format(i['main']['temp']),"°C",
           "> \r\nПогодные условия <", i['weather'][0]['description'],
-          "> \r\nСкорость ветра <", i['wind']['speed'],
+          "> \r\nСкорость ветра <", i['wind']['speed'], "м/c",
           "> \r\nВидимость <", i['visibility'] / 1000,"км >"
           )
